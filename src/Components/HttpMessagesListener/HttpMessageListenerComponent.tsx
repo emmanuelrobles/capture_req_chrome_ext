fd1ff8f084a03d86c788fc84677ebf68980c0202
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-09-15 17:10:52 -0400

    Added persistence

--- src/Components/HttpMessagesListener/HttpMessageListenerComponent.tsx
+++ src/Components/HttpMessagesListener/HttpMessageListenerComponent.tsx
@@ -1,50 +1,25 @@
 /*global chrome*/
 import React, {FC, useEffect, useState} from 'react';
-import {
-    BehaviorSubject,
-    combineLatest,
-    concatMap, distinct,
-    filter,
-    map,
-    mergeMap,
-    observable,
-    of,
-    Subject,
-    switchMap,
-    tap
-} from "rxjs";
 import {IRequestModel} from "../../Models/RequestModel";
-import {useObservable, useSubscription} from "observable-hooks";
-
-const REGEX_URLS : RegExp[] = [new RegExp(".*api-qa\\.junipermarket\\.com.*"),new RegExp(".*api-dev\\.junipermarket\\.com.*")]
-
-const observable$ = new Subject<IRequestModel>();
-const isListening$ = new BehaviorSubject<boolean>(false);
 
 const HttpMessageListenerComponent: FC<{onNewRequest: (request: IRequestModel) => void}> = ({onNewRequest}) => {
     const [isListening,setIsListening] = useState(false)
 
 
-    let request$ = useObservable(() =>
-        observable$.pipe(
-            distinct(req => req.time_stamp),
-            filter(req => isListening$.value && REGEX_URLS.some(rx => rx.test(req.url)))
-        )
-    )
-
-    useSubscription(request$,onNewRequest)
-    useSubscription(isListening$,v => {
-        setIsListening(v);
-        chrome.runtime.sendMessage({type:"listening",payload:v})
-    })
 
     useEffect(() => {
-
+        chrome.storage.local.get("listening", data => {
+            setIsListening(data.listening)
+        })
     },[])
+
+    useEffect(() => {
+       chrome.storage.local.set({listening:isListening})
+    },[isListening])
 
     return (
         <div>
-            <button onClick={() => isListening$.next(!isListening)}>Listening : {isListening ? "on" : "off"}</button>
+            <button onClick={() => {setIsListening(!isListening)}}>Listening : {isListening ? "on" : "off"}</button>
         </div>
     );
 };
--- src/background/background.ts
+++ src/background/background.ts
@@ -1,35 +1,62 @@
 /*global chrome*/
-
-import {distinct, Subject} from "rxjs";
+import {BehaviorSubject, distinct, distinctUntilChanged, filter, Subject, tap} from "rxjs";
+import {IRequestModel} from "../Models/RequestModel";
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({
-        listening:false,
-        requests: []
+    clearStorage()
+})
+
+let clearStorage = () => chrome.storage.local.set({
+    listening: false,
+    requests: []
+})
+
+const request$ = new Subject<IRequestModel>();
+const listening$ = new BehaviorSubject<boolean>(false);
+
+const REGEX_URLS: RegExp[] = [new RegExp(".*api-qa\\.junipermarket\\.com.*"), new RegExp(".*api-dev\\.junipermarket\\.com.*")]
+
+request$.pipe(
+    filter(e => listening$.value && REGEX_URLS.some(rx => rx.test(e.url))),
+    distinct(e => e.time_stamp)
+).subscribe(req => {
+    chrome.storage.local.get("requests", (data) => {
+        if (!chrome.runtime.lastError) {
+            chrome.storage.local.set({requests: [...data.requests, req]}, () => {
+                if (!chrome.runtime.lastError) {
+                }
+            })
+        }
+
     })
 })
 
-const obs = new Subject();
+listening$.pipe(
+    distinctUntilChanged()
+)
 
-obs.pipe(
-    distinct((e: any) => e.timestamp)
-).subscribe(e => {
-    console.log(e)
-})
 
-chrome.tabs.onUpdated.addListener((tabId,changeInfo,tab) => {
-    const REGEX_URLS = [new RegExp(".*qa\\.junipermarket\\.com.*"),new RegExp(".*dev\\.junipermarket\\.com.*")]
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
+        if (key === "listening") {
+            listening$.next(newValue)
+        }
+    }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    const REGEX_URLS = [new RegExp(".*qa\\.junipermarket\\.com.*"), new RegExp(".*dev\\.junipermarket\\.com.*")]
 
     // @ts-ignore
-    if(tab.status === "complete" && REGEX_URLS.some(rx => rx.test(tab.url))){
+    if (tab.status === "complete" && /^https?/.test(tab.url) && REGEX_URLS.some(rx => rx.test(tab.url))) {
         chrome.tabs.executeScript(tabId, {file: "contentScript.js"});
 
 
         chrome.runtime.onConnect.addListener(port => {
             port.onMessage.addListener((message) => {
-                chrome.storage.local.get("listening",data => {
-                    if(data.listening){
-                        // obs.next(message.payload)
+                chrome.storage.local.get("listening", data => {
+                    if (data.listening || true) {
+                        request$.next(message.payload)
                     }
                 })
             })
@@ -37,14 +64,10 @@ chrome.tabs.onUpdated.addListener((tabId,changeInfo,tab) => {
     }
 })
 
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if(request.type === "listening"){
-        chrome.storage.local.set({listening:request.payload},() => {
-            sendResponse({
-                type:"listening",
-                payload:request.payload
-            })
-        })
-    }
+chrome.runtime.onConnect.addListener(function (port) {
+    console.log("Connected .....");
+    port.onMessage.addListener(function (msg) {
+        console.log("message recieved", msg);
+        port.postMessage("Hi Popup.js");
+    });
 })
